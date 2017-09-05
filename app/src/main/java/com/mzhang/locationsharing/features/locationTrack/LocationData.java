a03c4c39f205e82615c3da94b63e5fd8f56a54db
Zhang, Chouting <michael.zhang@one.verizon.com>
2017-09-05 10:07:03 -0400

    Fixed merge error

--- app/src/main/java/com/mzhang/locationsharing/features/locationTrack/LocationData.java
+++ app/src/main/java/com/mzhang/locationsharing/features/locationTrack/LocationData.java
@@ -3,16 +3,11 @@ package com.mzhang.locationsharing.features.locationTrack;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
 /**
-<<<<<<< HEAD
  * Created by v579294 on 9/1/17.
-=======
- * Created by v579294 on 8/25/17.
->>>>>>> c299ee3435754d0f51687fd412a043f7960c2f5d
  */
 
 public class LocationData {
     private LatLng latLng;
-<<<<<<< HEAD
     private float deviceSpeed;
     private String roadSpeedLimit;
 
@@ -20,20 +15,10 @@ public class LocationData {
         this.latLng = new LatLng();
         deviceSpeed = 0;
         roadSpeedLimit = "";
-=======
-    private int speedLimit;
-    private int currentSpeed;
-
-    public LocationData() {
-        this.latLng = new LatLng();
-        this.speedLimit = 0;
-        this.currentSpeed = 0;
->>>>>>> c299ee3435754d0f51687fd412a043f7960c2f5d
     }
 
     public LocationData(LatLng latLng) {
         this.latLng = latLng;
-<<<<<<< HEAD
         deviceSpeed = 0;
         roadSpeedLimit = "";
     }
@@ -42,16 +27,6 @@ public class LocationData {
         this.latLng = latLng;
         this.deviceSpeed = deviceSpeed;
         this.roadSpeedLimit = roadSpeedLimit;
-=======
-        this.speedLimit = 0;
-        this.currentSpeed = 0;
-    }
-
-    public LocationData(LatLng latLng, int speedLimit, int currentSpeed) {
-        this.latLng = latLng;
-        this.speedLimit = speedLimit;
-        this.currentSpeed = currentSpeed;
->>>>>>> c299ee3435754d0f51687fd412a043f7960c2f5d
     }
 
     public LatLng getLatLng() {
@@ -62,7 +37,6 @@ public class LocationData {
         this.latLng = latLng;
     }
 
-<<<<<<< HEAD
     public float getDeviceSpeed() {
         return deviceSpeed;
     }
@@ -77,21 +51,5 @@ public class LocationData {
 
     public void setRoadSpeedLimit(String roadSpeedLimit) {
         this.roadSpeedLimit = roadSpeedLimit;
-=======
-    public int getSpeedLimit() {
-        return speedLimit;
-    }
-
-    public void setSpeedLimit(int speedLimit) {
-        this.speedLimit = speedLimit;
-    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
->>>>>>> c299ee3435754d0f51687fd412a043f7960c2f5d
     }
 }
